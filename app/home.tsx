4ff5757b9e7ccfcd3919907542834facd611979d
noparu <noparuuu@gmail.com>
2025-07-04 21:56:35 +0700

    add video in home page and add few updates

---
+++ app/home.tsx
@@ -0,0 +1,93 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useRef } from 'react'
+
+const HomePage = () => {
+    const videoRef = useRef(null)
+    const [videoStatus, setVideoStatus] = useState(false)
+    const [indicator, setIndicator] = useState(false)
+
+    // useEffect(() => {
+    //     setVideoStatus(videoRef?.current?.paused)
+    // }, [videoRef, indicator])
+
+    console.log("Video Paused= " + videoStatus)
+
+    // const handlePlayVideo = () => {
+    //     if (videoRef?.current) {
+    //         videoRef?.current?.play()
+    //         setIndicator(!indicator)
+    //     }
+    // }
+
+    // const handleStopVideo = () => {
+    //     if (videoRef?.current) {
+    //         videoRef?.current.pause()
+    //         setIndicator(false)
+    //     }
+    // }
+    return (
+        <div className='w-full h-dvh bg-zinc-800 relative overflow-clip'>
+            {/* === SUB CONTAINER === */}
+            <div className='w-full h-full flex items-center justify-center fade-in-animation'>
+                {/* === INNER CONTENT === */}
+                <div className='w-full h-full'>
+
+                    {/* === BACKGROUND LAYERS === */}
+                    <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center overflow-clip">
+                        <div className='absolute w-full h-dvh left-0 top-0 my-auto opacity-25 flex items-center justify-center'>
+                            <video className='w-full h-full object-cover' preload='none' autoPlay ref={videoRef} >
+                                <source src="/death-note-amv.mp4" type="video/mp4" />
+                                <track
+                                    src="/path/to/captions.vtt"
+                                    kind="subtitles"
+                                    srcLang="en"
+                                    label="English"
+                                />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+
+                        {/* Mist layers */}
+                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover fade-pulse-animation z-20" />
+                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover fade-pulse-animation z-20" />
+                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover fade-pulse-animation-2 z-20" />
+                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover fade-pulse-animation-2 z-20" />
+
+                        {/* Center heart image */}
+                        <img src='/kira-heart.png' className='w-[800px] zoom-in-animation' />
+                    </div>
+
+                    {/* === TEXT OVERLAY === */}
+                    <div className="relative w-full h-dvh flex flex-col items-center justify-center p-2 md:p-0">
+                        <div className="w-full flex flex-col items-center justify-center z-10 text-shadow-lg/100 opacity-65 text-shadow-zinc-800 zoom-in-animation">
+                            <h1>The Legend of Kira</h1>
+                            <h1>The Saviour</h1>
+                            <h3 className='mt-5'>Criminals worldwide</h3>
+                            <h3>Because Kira is among us again</h3>
+                            <h3>They are the one who will tolerate no wickedness</h3>
+                            <h3>Our messenger from hell</h3>
+                            <h3>Only those who believe in his existence and return</h3>
+                            <h3>MAY ENTER</h3>
+                            <h3>THIS SITE</h3>
+                        </div>
+
+                        {/* === ENTER BUTTON === */}
+                        <div className="z-30 mt-10 zoom-in-animation">
+                            <button >ENTER</button>
+                            {/* {videoStatus ?
+                                <button onClick={handlePlayVideo}>PLAY</button>
+                                :
+                                <button onClick={handleStopVideo}>STOP</button>
+                            } */}
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,20 @@
-'use client'
-import React from 'react'
+"use client"
+import { useState } from 'react'
+import HomePage from './home'
 
-const HomePage = () => {
+const MainHomePage = () => {
+    const [agree, setAgree] = useState(false)
     return (
-        <div id='container' className='w-full h-dvh bg-zinc-800 relative overflow-clip'>
-            <div id='sub-container' className='w-full h-full flex items-center justify-center fade-in'>
-                <div className='w-full h-full'>
-                    <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center overflow-clip">
-                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover animate-mist z-20" />
-                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover animate-mist z-20" />
-                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover animate-mist-2 z-20" />
-                        <img id="mis-1" src="/mist-1.jpg" className="absolute mix-blend-plus-lighter w-full h-dvh object-cover animate-mist-2 z-20" />
-                        <img src='/kira-heart.png' className='w-[800px] zoom-in' />
-                    </div>
-                    <div className="relative w-full h-dvh flex flex-col items-center justify-center">
-                        <div className="w-full flex flex-col items-center justify-center z-10 text-shadow-lg/100 opacity-65 text-shadow-zinc-500 zoom-in">
-                            <h1>The Legend of Kira</h1>
-                            <h1>The Saviour</h1>
-                            <h3 className='mt-5'>Criminals worldwide</h3>
-                            <h3>Because Kira is among us again</h3>
-                            <h3>They are the one who will tolerate no wickedness</h3>
-                            <h3>Our messenger from hell</h3>
-                            <h3>Only those who believe in his existence and return</h3>
-                            <h3>MAY ENTER</h3>
-                            <h3>THIS SITE</h3>
-                        </div>
-                        <div className="z-30 mt-10 zoom-in">
-                            <button onClick={() => alert('NIGGA')}>ENTER</button>
-                        </div>
-                    </div>
+        <div>
+            {!agree ?
+                <div className='bg-zinc-800 w-full h-dvh flex items-center justify-center text-center p-2 md:p-0'>
+                    <p className='w-[700px] text-white text-xl fade-in-animation-2'>This website was created out of my deep admiration for Death Note. I sincerely apologize if any aspects appear excessive. <span className='cursor-pointer text-red-500' onClick={() => setAgree(true)}>Would you like to proceed?</span></p>
                 </div>
-            </div>
+                :
+                <HomePage />
+            }
         </div>
     )
 }
 
-export default HomePage
+export default MainHomePage
